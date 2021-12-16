0f0384d18eea464900fa27195bbd3c22c1f2cac4
askwhyharsh <askwhyharsh@gmail.com>
2021-12-17 04:30:11 +0530

    deadline

--- pages/project/[id].js
+++ pages/project/[id].js
@@ -274,7 +274,7 @@ export default function Project() {
 										{project.state == 2 &&<span className="text-inter">Status :- Succesful</span>}
 									</li>
 									<li className="">
-										{deadline.timeZone}
+										Deadline - {deadline.timeZone}
 									</li>
 									<li className="">
 										<span className="text-inter">Raised {(Number(project.currentBalance)/1000000000000000000).toFixed(2)} MATIC</span>
